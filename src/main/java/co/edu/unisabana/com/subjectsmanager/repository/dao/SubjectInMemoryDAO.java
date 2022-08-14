8e1a55dc935996e61c926d2dd3f87b9d8d42bb89
Richard Guevara <leonvilla6@outlook.com>
2022-08-13 20:30:07 -0500

    Update SubjectInMemoryDAO.java

--- src/main/java/co/edu/unisabana/com/subjectsmanager/repository/dao/SubjectInMemoryDAO.java
+++ src/main/java/co/edu/unisabana/com/subjectsmanager/repository/dao/SubjectInMemoryDAO.java
@@ -1,5 +1,30 @@
 package co.edu.unisabana.com.subjectsmanager.repository.dao;
 
+import java.util.ArrayList;
+
+import co.edu.unisabana.com.subjectsmanager.repository.dto.SubjectDTO;
+
 public class SubjectInMemoryDAO implements ISubjectDAO {
+    private ArrayList<SubjectDTO> subjectDB;
+
+    @Override
+    public SubjectDTO createSubject(SubjectDTO subject) {
+        return null;
+    }
+
+    @Override
+    public void deleteSubject(String name) {
+
+    }
+
+    @Override
+    public SubjectDTO updateSubject(SubjectDTO subject) {
+        return null;
+    }
+
+    @Override
+    public SubjectDTO readSubject(String name) {
+        return null;
+    }
 
 }
