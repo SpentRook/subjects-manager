ae66e6dde70cc98beaaf7b0e2f838052812001f4
Jonathan Buitrago <jonathanbuitragoroncancio50@gmail.com>
2022-08-15 14:47:27 -0500

    Add attributes validation for update

--- src/main/java/co/edu/unisabana/com/subjectsmanager/service/subject/UpdateSubjectService.java
+++ src/main/java/co/edu/unisabana/com/subjectsmanager/service/subject/UpdateSubjectService.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import co.edu.unisabana.com.subjectsmanager.exception.NoContentException;
 import co.edu.unisabana.com.subjectsmanager.repository.SubjectRepository;
 import co.edu.unisabana.com.subjectsmanager.repository.dto.SubjectDTO;
+import co.edu.unisabana.com.subjectsmanager.repository.dto.SubjectDateDTO;
 
 public class UpdateSubjectService {
     private SubjectRepository subjectRepository;
@@ -15,9 +16,12 @@ public class UpdateSubjectService {
 
     public SubjectDTO run(String name, SubjectDTO subject) {
         ArrayList<SubjectDTO> subjectExists = this.subjectRepository.getSubjectByName(name);
+        int validCredits = subject.getCredits() == 0 ? subjectExists.get(0).getCredits() : subject.getCredits();
+        String validProfessor = subject.getProfessor() == null ? subjectExists.get(0).getProfessor() : subject.getProfessor();
+        ArrayList<SubjectDateDTO> validDates = subject.getDates() == null ? subjectExists.get(0).getDates() : subject.getDates();
         if (subjectExists.isEmpty())
             throw new NoContentException("The subject with name " + name + " was not found");
-        SubjectDTO newSubject = new SubjectDTO(name, subject.getCredits(), subject.getProfessor(), subject.getDates());
+        SubjectDTO newSubject = new SubjectDTO(name, validCredits, validProfessor , validDates);
         return this.subjectRepository.updateByName(newSubject);
     }
 }
